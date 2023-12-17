882673dbc7349abfb8811724e69f828fd8a4137d
eche-unicorn <d.anasteysha@gmail.com>
2023-12-18 00:48:09 +0200

    placeholders removed from node_modules

--- src/app/carousel-features.tsx
+++ src/app/carousel-features.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import Image from "next/image";
 import { Typography, Carousel } from "@material-tailwind/react";
-
+import Image from "next/image";
 
 export function CarouselFeatures() {
   return (
